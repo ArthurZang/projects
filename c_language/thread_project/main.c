81e423bc03b9324a18a2e2582a2ac198a425c3b7
ArthurZang <zangyansong0909@sohu.com>
2017-05-18 21:40:32 +0800

    race issue

--- c_language/thread_project/main.c
+++ c_language/thread_project/main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
+#define THREAD_NUM 50
+
+static int s_iCount;
 
 static void *thread_func(void *pArg)
 {
-    printf("hello pthread\n");
+    sleep(1);
+    s_iCount ++;
+
+    printf("hello pthread %d \n", s_iCount);
     return NULL;
 }
 int main(int argc, char *argv[])
 {
-    pthread_t threadID;
+    int i;
 
-    pthread_create(&threadID, NULL, &thread_func, NULL);
-    
-    pthread_join(threadID,NULL);
+    pthread_t threadID[THREAD_NUM];
+
+    for(i=0;i<THREAD_NUM;i++){
+        pthread_create(&threadID[i], NULL, &thread_func, NULL);
+    }
+
+    for(i=0;i<THREAD_NUM;i++)
+        pthread_join(threadID[i],NULL);
 
 }
